ac355536208b55b88c456b519271ec2e0144b5c0
Neil Gee <neil@wpbeaches.com>
2021-07-22 12:56:55 +1000

    gravity, woo myaccount styling

--- includes/unused-php.php
+++ includes/unused-php.php
@@ -4,16 +4,17 @@
 /**
  * Register/enqueue custom scripts and styles
  */
-//add_action( 'after_setup_theme', 'me_old_woo');
-function  me_old_woo() {
-	//add_action( 'wp_enqueue_scripts', 'bt_no_woo', 999);
+// add_action( 'after_setup_theme', 'me_old_woo');
+// function  me_old_woo() {
+	add_action( 'wp_enqueue_scripts', 'bt_no_woo', 999);
 	function bt_no_woo() {
 		wp_dequeue_style('bricks-woocommerce');
-		wp_enqueue_style('woocommerce-general');
-		wp_enqueue_style('woocommerce-layout');
-		wp_enqueue_style('woocommerce-smallscreen');
+		wp_enqueue_style('woocommerce-general', get_stylesheet_directory_uri() . '/css/woocommerce.css' );
+		wp_enqueue_style('woocommerce-layout', get_stylesheet_directory_uri() . '/css/woocommerce-layout.css' );
+		wp_enqueue_style('woocommerce-smallscreen', get_stylesheet_directory_uri() . '/css/woocommerce-smallscreen.css' );
+
 	}
-}
+// }
 
 add_action( 'init', function() {
 	var_dump( \Bricks\Database::$global_settings );
